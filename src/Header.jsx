c6a98a01ba23753a60ba0c2a5df6d2d363f0cc66
Fraser <Fraser.Hughes@btinternet.com>
2021-09-08 17:09:42 +0200

    Removed React from Hello

---
+++ src/Header.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Menu, Segment } from "semantic-ui-react";
+import { NavLink, Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <Segment inverted color="orange" >
+      <Menu inverted secondary size="medium" >
+        <Menu.Item
+          id="header"
+          name="my portfolio"
+          as={Link}
+          to={{ pathname: "/" }}
+          
+        />
+        <Menu.Item
+          id="about-tab"
+          name="about me"
+          as={NavLink}
+          to={{ pathname: "/about" }}
+        />
+        <Menu.Item
+          id="projects-tab"
+          name="projects"
+          as={NavLink}
+          to={{ pathname: "/projects" }}
+        />
+        <Menu.Item
+          id="contact-tab"
+          name="contact"
+          as={NavLink}
+          to={{pathname: "/contact"}}
+        />
+      </Menu>
+    </Segment>
+  );
+};
+
+export default Header;
