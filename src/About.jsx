1b9db19956ac662db1091117c6c02a5433b39981
Fraser <Fraser.Hughes@btinternet.com>
2021-09-09 15:21:03 +0200

    Change Project tab from class to component

--- src/About.jsx
+++ src/About.jsx
@@ -5,6 +5,11 @@ const About = () => {
   return (
     <Container>
       <h1 id="about-header">About Me</h1>
+      <img
+        id="about-picture"
+        src="https://ibb.co/nkTS6SG"
+        alt="Picture of me!"
+      />
       <p>
         My journey into web developement started with the{" "}
         <a
@@ -20,16 +25,18 @@ const About = () => {
         more content.
       </p>
       <p>
-        I grew up on the south east coast of the UK before spend six years in
+        I grew up on the south east coast of the UK before spending six years in
         Bristol going to university and working as an Aerospace Engineer. I
         moved out to Sweden in December 2020, and haven't looked back,
-        Gothenburg is a great city and I plan on spending a lot time here. While
-        out here I decided to switch careers, and move into something completely
-        new, which is where I discoverd Craft Academy and my journey began.
+        Gothenburg is a great city and I plan on spending a lot time here. After
+        moving out here I decided to switch careers, and move into something
+        completely new, which is where I discoverd Craft Academy and my journey
+        began.
       </p>
       <p>
-        Outside of coding I have hobbies including running, making use of Gothenburgs
-        amazing nature, försöker lära sig svenska, and cooking as a way to relax.
+        Outside of coding I have hobbies including running, making use of
+        Gothenburgs amazing nature, försöker lära sig svenska, and cooking as a
+        way to relax.
       </p>
     </Container>
   );
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,37 +1,63 @@
 import axios from "axios";
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { Container, Grid } from "semantic-ui-react";
 import ProjectCard from "./ProjectCard";
 
-class Projects extends Component {
-  state = {
-    projects: [],
-  };
+const Projects = () => {
+  const [project] = useState({});
+  const [projects, setProject] = useEffect([]);
 
-  componentDidMount() {
+  useEffect(() => {
+    //axios call in here
     axios.get("./data/projects.json").then((response) => {
       this.setState({ projects: response.data });
-    });
-  }
-
-  render() {
-    const { projects } = this.state;
-
-    let projectsList = projects.map((project) => {
-      return (
-        <div id={`project-${project.id}`} key={project.id}>
-          <ProjectCard project={project} />
-        </div>
-      );
-    });
+    }, []);
 
     return (
-      <Container>
-        <h1 id="projects-header">My Projects</h1>
-        <Grid>{projectsList}</Grid>
-      </Container>
+      <React.Fragment id={`project-${project.id}`} key={project.id}>
+        <ProjectCard project={project} />
+      </React.Fragment>
     );
-  }
-}
-
+  });
+};
 export default Projects;
+
+//---------------------------------------------------------------------------------------
+
+// import axios from "axios";
+// import React, { Component } from "react";
+// import { Container, Grid } from "semantic-ui-react";
+// import ProjectCard from "./ProjectCard";
+
+// class Projects extends Component {
+//   state = {
+//     projects: [],
+//   };
+
+//   componentDidMount() {
+//     axios.get("./data/projects.json").then((response) => {
+//       this.setState({ projects: response.data });
+//     });
+//   }
+
+//   render() {
+//     const { projects } = this.state;
+
+// let projectsList = projects.map((project) => {
+//   return (
+//     <div id={`project-${project.id}`} key={project.id}>
+//       <ProjectCard project={project} />
+//     </div>
+//   );
+// });
+
+//     return (
+//       <Container>
+//         <h1 id="projects-header">My Projects</h1>
+//         <Grid>{projectsList}</Grid>
+//       </Container>
+//     );
+//   }
+// }
+
+// export default Projects;
